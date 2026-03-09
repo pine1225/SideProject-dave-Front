42b2d2cb3e871b522d9e7c1dc8cb4771a69643ef
pine1225 <112848935+coke709@users.noreply.github.com>
2026-03-09 14:26:29 +0900

    search page 수정(1)

--- src/app/(with-nevigator)/data/marinca/[id]/page.tsx
+++ src/app/(with-nevigator)/data/marinca/[id]/page.tsx
@@ -4,6 +4,7 @@ import GameMarincaCategory from "@/app/components/game-marinca-category";
 import Searchbar from "@/app/components/searchbar";
 import { notFound, useSearchParams } from "next/navigation";
 import style from "./page.module.css";
+import Image from "next/image";
 
 export default async function Page({
   params,
@@ -13,7 +14,7 @@ export default async function Page({
   const { id } = await params;
 
   const response = await fetch(
-    `${process.env.BOOT_API_SERVER_URL}/api/data/marinca/${id}`
+    `${process.env.BOOT_API_SERVER_URL}/api/data/marinca/${id}`,
   );
 
   if (!response.ok) {
@@ -27,12 +28,25 @@ export default async function Page({
   const { data, count, message, category } = categoryData;
 
   return (
-    <div className={style.container}>
-      <GameDataTag />
-      <div className={style.item_container}>
-        <GameMarincaCategory />
-        <Searchbar />
-        <GameItem {...categoryData} />
+    <div>
+      <div className={style.banner_outer}>
+        <div className={style.image_wrap}>
+          <Image
+            fill
+            priority
+            alt="img"
+            quality={90}
+            src="/assets/game-img-download/No_Logo_Ver/DaveTheDiver_ScreenShot_003.png"
+          />
+        </div>
+      </div>
+      <div className={style.container}>
+        <GameDataTag />
+        <div className={style.item_container}>
+          <GameMarincaCategory />
+          <Searchbar />
+          <GameItem {...categoryData} />
+        </div>
       </div>
     </div>
   );
